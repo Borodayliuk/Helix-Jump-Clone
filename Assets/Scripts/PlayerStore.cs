9f4ed22e963766e33a19f19297af8731df8b1410
GeeksforGeeks <nickborodayliuk@gmail.com>
2022-08-24 21:25:39 +0300

    Test 3

--- Assets/Scripts/PlayerStore.cs
+++ Assets/Scripts/PlayerStore.cs
@@ -7,9 +7,11 @@ public class PlayerStore : MonoBehaviour
     [SerializeField] int a;
     void Start()
     {
-        
+        a = 0;
     }
-
+    public void Test() {
+        a = 1;
+    }
     // Update is called once per frame
     void Update()
     {
